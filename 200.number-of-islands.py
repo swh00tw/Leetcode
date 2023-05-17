ff9554a7d7243b00fc417c5d0fd3da50d6ce1c22
swh00tw <a6140000@gmail.com>
2023-05-17 12:05:26 +0800

    update solution

--- 200.number-of-islands.py
+++ 200.number-of-islands.py
@@ -10,31 +10,32 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
         islands = 0
-        visitedOnes = set()
-        def isUnvisitedOne(x, y):
-            return grid[x][y]=="1" and (x, y) not in visitedOnes
+        unVisitedOnes = set()
         for i in range(n):
             for j in range(m):
-                if isUnvisitedOne(i, j):
-                    # bfs
-                    islands += 1
-                    s = [(i, j)]
-                    visitedOnes.add((i, j))
-                    while s:
-                        x, y = s.pop(0)
-                        # append adjacent nodes in stack if there have value equal to "1"
-                        if y-1>=0 and isUnvisitedOne(x, y-1):
-                            s.append((x, y-1))
-                            visitedOnes.add((x, y-1))
-                        if y+1<m and isUnvisitedOne(x, y+1):
-                            s.append((x, y+1))
-                            visitedOnes.add((x, y+1))
-                        if x-1>=0 and isUnvisitedOne(x-1, y):
-                            s.append((x-1, y))
-                            visitedOnes.add((x-1, y))
-                        if x+1<n and isUnvisitedOne(x+1, y):
-                            s.append((x+1, y))
-                            visitedOnes.add((x+1, y))
+                if grid[i][j]=="1":
+                    unVisitedOnes.add((i, j))
+        def isUnvisitedOne(x, y):
+            return grid[x][y]=="1" and (x, y) in unVisitedOnes
+        while len(unVisitedOnes)>0:
+            randomNode = unVisitedOnes.pop()
+            islands+=1
+            # bfs
+            s = [randomNode]
+            while s:
+                x, y = s.pop(0)
+                if y-1>=0 and isUnvisitedOne(x, y-1):
+                    s.append((x, y-1))
+                    unVisitedOnes.discard((x, y-1))
+                if y+1<m and isUnvisitedOne(x, y+1):
+                    s.append((x, y+1))
+                    unVisitedOnes.discard((x, y+1))
+                if x-1>=0 and isUnvisitedOne(x-1, y):
+                    s.append((x-1, y))
+                    unVisitedOnes.discard((x-1, y))
+                if x+1<n and isUnvisitedOne(x+1, y):
+                    s.append((x+1, y))
+                    unVisitedOnes.discard((x+1, y))
         return islands
         
 # @lc code=end
