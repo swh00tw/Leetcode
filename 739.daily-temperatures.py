000dde23b22ee16f3326da33c9f590d386210fc0
swh00tw <a6140000@gmail.com>
2023-07-04 12:35:31 +0800

    finish daily

--- 739.daily-temperatures.py
+++ 739.daily-temperatures.py
@@ -4,24 +4,23 @@
 # [739] Daily Temperatures
 #
 
+
 # @lc code=start
+from collections import deque
+
+
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # create a stack s --> the day element in stack means they are waiting for a warmer day
-        # parse t in temperatures, 
-        # when t <= s.top[0], push (t, index) onto stack
-        # else, pop the top until t<=s.top[0]
-        # each time pop the top item from stack, record the index difference(number of days to wait)
-        s = []
-        res = [0]*len(temperatures)
-        for i, t in enumerate(temperatures):
-            if len(s)!=0 and t>s[-1][0]:
-                while len(s)>0 and t>s[-1][0]:
-                    day = s.pop()
-                    indexDiff = i-day[1]
-                    res[day[1]] = indexDiff
-            s.append([t, i])
-        return res
-        
-# @lc code=end
+        # store (temp, index) pair
+        # the temp must in decreasing order
+        ans = [0] * len(temperatures)
+        stack = deque([])
+        for i, temp in enumerate(temperatures):
+            while len(stack) > 0 and stack[-1][0] < temp:
+                _, waitDayIdx = stack.pop()
+                ans[waitDayIdx] = i - waitDayIdx
+            stack.append((temp, i))
+        return ans
 
+
+# @lc code=end
