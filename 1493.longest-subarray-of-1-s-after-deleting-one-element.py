238c33f3689e59e61b6ba13ef1aaf36d9e92de31
swh00tw <a6140000@gmail.com>
2023-07-05 23:36:15 +0800

    finish daily

--- 1493.longest-subarray-of-1-s-after-deleting-one-element.py
+++ 1493.longest-subarray-of-1-s-after-deleting-one-element.py
@@ -4,40 +4,32 @@
 # [1493] Longest Subarray of 1's After Deleting One Element
 #
 
+
 # @lc code=start
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        # two pointers, keep counting acc 1s
-        # if encounter 1, increment and move right pointer
-        # if encounter 0 and have chance, skip
-        # if encounter 0 and don't have chance, restore the chance we used before (update left pointer to its right)
-        chance = True
-        acc = 0
+        # we got one chance to skip one zero
+        # if we encounter zero and have no chance left
+        # restore the chance to skip from the last time we used it
+        # need to recalculate ones from the previous zero
+        # so move start pointer and minus the unvalid one's
         best = 0
-        l, r=0, 0
-        while r<len(nums):
-            if nums[r]==1:
-                r += 1
-                acc += 1
-            elif nums[r]==0 and chance:
-                chance = not chance
-                r+=1
+        count = 0
+        chance = 1
+        startIdx = 0
+        for i, n in enumerate(nums):
+            if n == 1:
+                count += 1
+                best = max(best, count)
+            elif n == 0 and chance == 1:
+                chance -= 1
+                continue
             else:
-                # save best score now
-                best = max(best, acc)
-                # move l to the first 0's right
-                while l<len(nums):
-                    if nums[l]==1:
-                        acc-=1
-                    elif nums[l]==0:
-                        l+=1
-                        break
-                    l+=1
-                r+=1
-        best = max(best, acc)
-        return best-1 if chance else best
+                while nums[startIdx] != 0:
+                    startIdx += 1
+                    count -= 1
+                startIdx += 1  # move to the right of the zero
+        return best - 1 if chance == 1 else best
 
-                
-        
+
 # @lc code=end
-
