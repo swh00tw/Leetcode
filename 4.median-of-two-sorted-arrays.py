c39b126c126596b61e6c9aa56bb42c9a243e0321
swh00tw <a6140000@gmail.com>
2023-08-14 13:23:13 -0400

    finish daily

---
+++ 4.median-of-two-sorted-arrays.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode id=4 lang=python3
+#
+# [4] Median of Two Sorted Arrays
+#
+
+
+# @lc code=start
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # sol: https://www.youtube.com/watch?v=q6IEA26hvXc
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
+        if len(B) < len(A):
+            A, B = B, A
+
+        # A is smaller array
+        # B is larger array
+        l, r = 0, len(A) - 1
+        while True:
+            i = (l + r) // 2  # A
+            j = half - i - 2  # B
+
+            Aleft = A[i] if i >= 0 else float("-inf")
+            Aright = A[i + 1] if (i + 1) < len(A) else float("inf")
+            Bleft = B[j] if j >= 0 else float("-inf")
+            Bright = B[j + 1] if (j + 1) < len(B) else float("inf")
+
+            # partition is correct
+            if Aleft <= Bright and Bleft <= Aright:
+                # odd
+                if total % 2:
+                    return min(Aright, Bright)
+                # even
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                r = i - 1
+            else:
+                l = i + 1
+
+
+# @lc code=end
