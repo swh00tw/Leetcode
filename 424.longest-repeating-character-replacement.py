1c3a51f37a0b266c82a6cd71f22c50b65869a687
swh00tw <a6140000@gmail.com>
2023-08-21 17:30:52 -0400

    finish daily

--- 424.longest-repeating-character-replacement.py
+++ 424.longest-repeating-character-replacement.py
@@ -8,9 +8,6 @@
 # @lc code=start
 # ref: https://leetcode.com/problems/longest-repeating-character-replacement/solutions/3524144/python-code-explained-with-approach-and-time-complexity/
 class Solution:
-    def __init__(self):
-        self.m = 0
-
     def characterReplacement(self, s: str, k: int) -> int:
         # init two pointers l, r to 0
         # create a var maxf record the maxFrequency in sliding window
@@ -21,14 +18,15 @@ class Solution:
         l = 0
         n = len(s)
         d = {}
+        ans = 0
         for r in range(n):
             d[s[r]] = 1 + d.get(s[r], 0)
             maxf = max(d.values())
             if (r - l + 1) - maxf > k:
                 d[s[l]] -= 1
                 l += 1
-            self.m = max(self.m, r - l + 1)
-        return self.m
+            ans = max(ans, r - l + 1)
+        return ans
 
 
 # @lc code=end
