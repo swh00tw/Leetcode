49284aef804430874166e96ac9907b421971996e
swh00tw <a6140000@gmail.com>
2023-09-03 12:11:01 -0400

    finish daily

--- 239.sliding-window-maximum.py
+++ 239.sliding-window-maximum.py
@@ -11,26 +11,34 @@ from collections import deque
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # ref: https://leetcode.com/problems/sliding-window-maximum/solutions/3915610/100-deque-o-n-performance-optimal/
-        # maintain a deque that first element is always the largest value
-        # when processing a new element,
-        # if a new element is added at the end, remove element in the front which has a lower value
-        # since those are useless elements
-        # if the first element is out of window, remove it
-        deq = deque()  # store (idx, val) pair
-        res = []
+        # use a deque to store (val, idx) in the window
+        # the queue is in descending order, maintained by the following rule:
+        # When a new num is being added,
+        # keep elminate num from the end if the top val of queue is lower than new num
+        # since the previous lower value is useless,
+        # it is impossible to be max and it will disappear earlier than the new num
+        # so it's no reason for it to be store in the queue
+        # When sliding window move,
+        # pop one from the start of the queue if needed (check idx)
+        # by doing so, the maximum value of window is at the front of the queue
+        q = deque()
+        for i in range(k):
+            while q and q[-1][0] <= nums[i]:
+                q.pop()
+            q.append((nums[i], i))
 
-        for i in range(len(nums)):
-            newValue = nums[i]
-            if deq and deq[0][0] < i - k + 1:
-                deq.popleft()
-            while deq and deq[-1][1] < newValue:
-                deq.pop()
-            deq.append((i, newValue))
-
-            res.append(deq[0][1])
-
-        return res[k - 1 :]
+        ans = []
+        ans.append(q[0][0])
+        for right in range(k, len(nums)):
+            left = right + 1 - k
+            if q[0][1] < left:
+                q.popleft()
+            newNum = nums[right]
+            while q and q[-1][0] <= newNum:
+                q.pop()
+            q.append((newNum, right))
+            ans.append(q[0][0])
+        return ans
 
 
 # @lc code=end
