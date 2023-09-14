cb11f8d3ddfc56a981ae4829e18ca48378572891
swh00tw <a6140000@gmail.com>
2023-09-14 14:26:07 -0400

    finish weighted interval schedule

---
+++ 1235.maximum-profit-in-job-scheduling.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=1235 lang=python3
+#
+# [1235] Maximum Profit in Job Scheduling
+#
+
+# @lc code=start
+from functools import lru_cache
+import bisect
+
+
+class Solution:
+    def jobScheduling(
+        self, startTime: List[int], endTime: List[int], profit: List[int]
+    ) -> int:
+        # top down dp
+        # sort a job by ending time
+        # for a new job if it has overlap, not choose it
+        # if if can be included, either choose it or skip
+        jobs = []
+        for i in range(len(startTime)):
+            jobs.append((startTime[i], endTime[i], profit[i]))
+        jobs.sort(key=lambda x: x[0])
+        sortedStart = [x[0] for x in jobs]
+
+        @lru_cache(None)
+        def maxProfit(idx):
+            if idx >= len(startTime):
+                return 0
+
+            # skip or include
+            skip = maxProfit(idx + 1)
+            _, end, profit = jobs[idx]
+            nextIdx = bisect.bisect_left(sortedStart, end)
+            include = maxProfit(nextIdx) + profit
+            return max(skip, include)
+
+        return maxProfit(0)
+
+
+# @lc code=end
