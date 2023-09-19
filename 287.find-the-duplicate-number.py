41d0ad21d775cdbb01602d5beba9dc631de1fc0c
swh00tw <a6140000@gmail.com>
2023-09-19 13:39:16 -0400

    finish daily

--- 287.find-the-duplicate-number.py
+++ 287.find-the-duplicate-number.py
@@ -9,18 +9,23 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         # tortoise-hare algorithm
-        slow, fast = nums[0], nums[0]
+        # val is next index
+        # find the start of the cycle
+        # the prev node's val is the duplicate
+        slow, fast = 0, 0
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
-            if fast == slow:
+            if slow == fast:
                 break
 
-        fast = nums[0]
-        while fast != slow:
-            fast = nums[fast]
+        # run at same speed
+        slow = 0
+        while True:
             slow = nums[slow]
-        return fast
+            fast = nums[fast]
+            if slow == fast:
+                return slow
 
 
 # @lc code=end
