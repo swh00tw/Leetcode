72f02284a3a8d6372ca1b2f382fc46a8ae6e1a74
swh00tw <a6140000@gmail.com>
2023-12-27 10:14:26 +0800

    Refactor isPalindrome function to improve readability and performance

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -8,33 +8,22 @@
 # @lc code=start
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        n = len(s)
-        l, r = 0, n - 1
-        while l <= r and l < n and r >= 0:
-            leftChar = self.getChar(s[l])
-            if not leftChar:
+        l, r = 0, len(s) - 1
+        while True:
+            # get next l
+            while l < len(s) and not s[l].isalnum():
                 l += 1
-                continue
-            rightChar = self.getChar(s[r])
-            if not rightChar:
+            # get next r
+            while r > 0 and not s[r].isalnum():
                 r -= 1
-                continue
+            if l > r:
+                return True
 
-            if leftChar != rightChar:
+            if s[l].lower() != s[r].lower():
                 return False
             l += 1
             r -= 1
         return True
-
-    def getChar(self, char):
-        if "a" <= char <= "z":
-            return char
-        if "A" <= char <= "Z":
-            return char.lower()
-        if char.isnumeric():
-            return char
-
-        return None
 
 
 # @lc code=end
