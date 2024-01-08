6a173d5b8ccadda87834d5cab6185a0b229af3b2
swh00tw <a6140000@gmail.com>
2024-01-08 10:59:05 +0800

    Refactor addTwoNumbers method to improve readability and maintainability

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -15,30 +15,34 @@ class Solution:
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        p1 = l1
-        p2 = l2
+        n1 = l1
+        n2 = l2
+        res = ListNode()
+        tail = res
         addOne = False
-        prevRes = ListNode()
-        curr = prevRes
-        while p1 or p2:
-            val1 = p1.val if p1 is not None else 0
-            val2 = p2.val if p2 is not None else 0
-            val = val1 + val2
-            if addOne is True:
-                addOne = False
-                val += 1
-            addOne = val >= 10
-            val %= 10
-            newNode = ListNode(val)
-            curr.next = newNode
-            curr = curr.next
-            if p1 is not None:
-                p1 = p1.next
-            if p2 is not None:
-                p2 = p2.next
-        if addOne is True:
-            curr.next = ListNode(1)
-        return prevRes.next
+        while n1 or n2:
+            # add n1 and n2, generate a new Node
+            newNode, addOne = self.addTwoNode(n1, n2, addOne)
+            # append to tail, update tail
+            tail.next = newNode
+            tail = tail.next
+            # update n1, n2
+            n1 = n1.next if n1 else None
+            n2 = n2.next if n2 else None
+        # if addOne is true, append last node
+        if addOne:
+            tail.next = ListNode(1)
+        return res.next
+
+    def addTwoNode(
+        self, n1: Optional[ListNode], n2: Optional[ListNode], addOne=False
+    ) -> Optional[ListNode]:
+        if not n1 and not n2:
+            return None
+        val1 = n1.val if n1 is not None else 0
+        val2 = n2.val if n2 is not None else 0
+        newVal = val1 + val2 + (1 if addOne else 0)
+        return ListNode(newVal % 10), (newVal // 10 == 1)
 
 
 # @lc code=end
