32af9e8403fa164227da8128397fef1811aac971
swh00tw <a6140000@gmail.com>
2024-01-29 15:26:29 -0500

    Refactor numIslands method to use BFS and improve code readability

--- 200.number-of-islands.py
+++ 200.number-of-islands.py
@@ -4,39 +4,39 @@
 # [200] Number of Islands
 #
 
+
 # @lc code=start
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # run BFS for unvisited cells (value is "1" && not visited)
+        #     while running, mark cell as visited when the value of cell is "1"
+        # count how many island
+        deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         n = len(grid)
         m = len(grid[0])
         islands = 0
-        unVisitedOnes = set()
+        visited = [[False] * m for _ in range(n)]
         for i in range(n):
             for j in range(m):
-                if grid[i][j]=="1":
-                    unVisitedOnes.add((i, j))
-        def isUnvisitedOne(x, y):
-            return grid[x][y]=="1" and (x, y) in unVisitedOnes
-        while len(unVisitedOnes)>0:
-            randomNode = unVisitedOnes.pop()
-            islands+=1
-            # bfs
-            s = [randomNode]
-            while s:
-                x, y = s.pop(0)
-                if y-1>=0 and isUnvisitedOne(x, y-1):
-                    s.append((x, y-1))
-                    unVisitedOnes.discard((x, y-1))
-                if y+1<m and isUnvisitedOne(x, y+1):
-                    s.append((x, y+1))
-                    unVisitedOnes.discard((x, y+1))
-                if x-1>=0 and isUnvisitedOne(x-1, y):
-                    s.append((x-1, y))
-                    unVisitedOnes.discard((x-1, y))
-                if x+1<n and isUnvisitedOne(x+1, y):
-                    s.append((x+1, y))
-                    unVisitedOnes.discard((x+1, y))
+                # if not visited and value is "1"
+                if grid[i][j] == "1" and not visited[i][j]:
+                    islands += 1
+                    # BFS
+                    queue = [(i, j)]
+                    visited[i][j] = True
+                    while queue:
+                        x, y = queue.pop(0)
+                        for dx, dy in deltas:
+                            nx, ny = x + dx, y + dy
+                            if (
+                                0 <= nx < n
+                                and 0 <= ny < m
+                                and grid[nx][ny] == "1"
+                                and not visited[nx][ny]
+                            ):
+                                visited[nx][ny] = True
+                                queue.append((nx, ny))
         return islands
-        
-# @lc code=end
 
+
+# @lc code=end
