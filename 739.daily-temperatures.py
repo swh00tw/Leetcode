d68e4abd52e02f00580a3a6e1b2155f143c5c29c
swh00tw <a6140000@gmail.com>
2024-01-31 15:32:35 -0500

    Refactor dailyTemperatures method to use a stack for improved efficiency

--- 739.daily-temperatures.py
+++ 739.daily-temperatures.py
@@ -6,20 +6,21 @@
 
 
 # @lc code=start
-from collections import deque
-
-
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # store (temp, index) pair
-        # the temp must in decreasing order
-        ans = [0] * len(temperatures)
-        stack = deque([])
-        for i, temp in enumerate(temperatures):
-            while len(stack) > 0 and stack[-1][0] < temp:
-                _, waitDayIdx = stack.pop()
-                ans[waitDayIdx] = i - waitDayIdx
-            stack.append((temp, i))
+        # maintain a stack, and the temperature inside only gets lower or equal
+        # store (tempature, index) inside
+        # when parse a new tempature, try push it into stack
+        # pop and process if the top one is cooler
+        stack = []
+        n = len(temperatures)
+        ans = [0] * n
+        for idx, temp in enumerate(temperatures):
+            # push
+            while stack and stack[-1][0] < temp:
+                _, targetIdx = stack.pop()
+                ans[targetIdx] = idx - targetIdx
+            stack.append((temp, idx))
         return ans
 
 
