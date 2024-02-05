584a6cb394503db64f386bf4377b1bd3d4aa2936
swh00tw <a6140000@gmail.com>
2024-02-05 14:01:46 -0500

    Refactor calcEquation method in Solution class

--- 399.evaluate-division.py
+++ 399.evaluate-division.py
@@ -6,43 +6,39 @@
 
 # @lc code=start
 from collections import defaultdict
-class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        # create a graph
-        # for equations[i] = [a, b], a/b = values[i]
-        # we can create an edge from a to bi and the weight is values[i]
-        # and an edge from b to a, the weight is inverse number
-        # then, if we want a/c, we can start from a and go to c. the answer can be obtained by multuply all weight along the path
-        adjList = defaultdict(list)
-        for i in range(len(equations)):
-            src, dst = equations[i]
-            weight = values[i]
-            adjList[src].append((dst, weight))
-            adjList[dst].append((src, 1/weight))
-        # print(adjList)
-        return [self.getAnswer(adjList, src, dst) for src, dst in queries]
-        
-    def getAnswer(self, adjList, src, dst):
-        if src not in adjList or dst not in adjList:
-            return -1.0
-        if src==dst:
-            return 1.0
-        # bfs
-        visited = {}
-        for k in adjList:
-            visited[k]=False
-        s = [(src, 1)]
-        visited[src]=True
-        while s:
-            curr, currVal = s.pop(0)
-            # print("src: ", src, "dst: ", curr, "currVal: ",currVal)
-            if curr==dst:
-                return currVal
-            for dstNode, weight in adjList[curr]:
-                if not visited[dstNode]:
-                    visited[dstNode]=True
-                    s.append((dstNode, currVal*weight))
-        return -1.0
-        
-# @lc code=end
 
+
+class Solution:
+    def calcEquation(
+        self, equations: List[List[str]], values: List[float], queries: List[List[str]]
+    ) -> List[float]:
+        # build the graph using equation,
+        # build adjList, store (neighbor, weight)
+        self.adjList = defaultdict(list)
+        for i, pair in enumerate(equations):
+            weight = values[i]
+            src, dst = pair
+            self.adjList[src].append((dst, weight))
+            self.adjList[dst].append((src, 1 / weight))
+        return [self.getAns(src, dst) for src, dst in queries]
+
+    def getAns(self, src, dst):
+        if src not in self.adjList or dst not in self.adjList:
+            return -1.0
+        if src == dst:
+            return 1.0
+        # run DFS + backtracking to get answer
+        # backtracking: remove this node before entering deeper recursion to avoid duplicate
+        # remember to add back before any return to make sure the graph will be remain the same after each call
+        key, arr = src, self.adjList[src]
+        del self.adjList[src]
+        for neighbor, weight in arr:
+            ans = weight * self.getAns(neighbor, dst)
+            if ans >= 0:
+                self.adjList[key] = arr
+                return ans
+        self.adjList[key] = arr
+        return -1.0
+
+
+# @lc code=end
