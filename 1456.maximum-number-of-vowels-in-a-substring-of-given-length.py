d80cdaee9dfa87dbccaff8590a043a192707f7e2
swh00tw <a6140000@gmail.com>
2024-06-15 23:21:55 -0400

    Refactor maxVowels method in Solution class for improved efficiency

--- 1456.maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456.maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -4,29 +4,36 @@
 # [1456] Maximum Number of Vowels in a Substring of Given Length
 #
 
+
 # @lc code=start
-def isVowel(char):
-    if len(char)>1:
-        raise Exception("isVowel's input should be one char")
-    vowels = "aeiou"
-    return char in vowels
+
+
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        i = 0
-        window = s[i:i+k]
-        count = 0
-        for c in window:
-            if isVowel(c):
-                count+=1
-        best = count
-        i+=1
-        while i<len(s) and i+k<=len(s):
-            count += (1 if isVowel(s[i+k-1]) else 0)
-            count -= (1 if isVowel(s[i-1]) else 0)
-            best = max(best, count)
-            i+=1
-        return best
-        
-        
-# @lc code=end
+        # sliding window
+        # need a function isVowel
+        # keep track of current numVowels and bestAns
+        numVowels = 0
+        bestAns = 0
+        n = len(s)
+        for i in range(k):
+            if self.isVowel(s[i]):
+                numVowels += 1
+        if n == k:
+            return numVowels
+        bestAns = numVowels
+        for i in range(k, n):
+            if self.isVowel(s[i]):
+                numVowels += 1
+            if self.isVowel(s[i - k]):
+                numVowels -= 1
+            bestAns = max(bestAns, numVowels)
+        return bestAns
 
+    def isVowel(self, char):
+        if len(char) > 1:
+            raise Exception("the argument must be a character")
+        return char in "aeiou"
+
+
+# @lc code=end
