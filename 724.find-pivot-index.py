fac3b1f9f56aac867f9afedf14757bac10fb4e49
swh00tw <a6140000@gmail.com>
2024-06-19 16:16:58 -0400

    Refactor pivotIndex method in Solution class for improved efficiency and readability

--- 724.find-pivot-index.py
+++ 724.find-pivot-index.py
@@ -4,22 +4,23 @@
 # [724] Find Pivot Index
 #
 
+
 # @lc code=start
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        n = len(nums)
-        # apply running sum first
-        runningSums = []
-        currSum = 0
-        for num in nums:
-            currSum+=num
-            runningSums.append(currSum)
-        for i, runningSum in enumerate(runningSums):
-            leftSum = 0 if i==0 else runningSums[i-1]
-            rightSum = 0 if i == n-1 else runningSums[-1]-runningSum
-            if leftSum == rightSum:
+        # calculate running sum, sums[i] = nums[0] + ... + nums[i]
+        curr = 0
+        sums = []
+        for n in nums:
+            curr += n
+            sums.append(curr)
+        # find pivot index
+        for i, n in enumerate(nums):
+            left_sum = 0 if i == 0 else sums[i - 1]
+            right_sum = 0 if i == len(nums) - 1 else (sums[-1] - sums[i])
+            if left_sum == right_sum:
                 return i
         return -1
-        
-# @lc code=end
 
+
+# @lc code=end
