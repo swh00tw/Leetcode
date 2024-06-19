5dea63c3359bd0d3ce9ef1e18cf7d57f230fe523
swh00tw <a6140000@gmail.com>
2024-06-19 15:59:34 -0400

    Refactor longestSubarray method in Solution class for improved efficiency and readability

--- 1493.longest-subarray-of-1-s-after-deleting-one-element.py
+++ 1493.longest-subarray-of-1-s-after-deleting-one-element.py
@@ -8,22 +8,32 @@
 # @lc code=start
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        # we got one chance to skip one zero
-        # keep track of prev seen zero's index and the start counting index
-        # if we encounter zero except for the first zero, update startCountingIdx and prevZeroIdx
-        # if every step, count the length of the window between current idx and startCountingIdx
+        # have one chance to skip a zero
+        # sliding window
+        # left pointer always point to the left of the window
+        # if have no chance and encounter next zero
+        # move left pointer to the right of the zero (recycle the power of skipping zero)
         best = 0
+        n = len(nums)
+        left = 0
         chance = 1
-        prevZeroIdx = 0
-        startCountingIdx = 0
-        for i, n in enumerate(nums):
-            if n == 0 and chance == 1:
-                chance -= 1
-                prevZeroIdx = i
-            elif n == 0 and chance == 0:
-                startCountingIdx = prevZeroIdx + 1
-                prevZeroIdx = i
-            best = max(best, i - startCountingIdx)
+        for right in range(n):
+            num = nums[right]
+            if num == 0:
+                if chance == 1:
+                    chance = 0
+                else:
+                    # recycle
+                    while left <= right:
+                        if nums[left] == 0:
+                            break
+                        left += 1
+                    left += 1
+            best = max(best, right + 1 - left - (1 if chance == 0 else 0))
+
+        # must delete an element even if no zero
+        if chance == 1:
+            return best - 1
         return best
 
 
