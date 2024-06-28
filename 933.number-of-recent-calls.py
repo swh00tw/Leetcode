85941085bbd94bef8fdd4265a55850731257fb63
swh00tw <a6140000@gmail.com>
2024-06-27 21:18:58 -0400

    Refactor RecentCounter class for improved efficiency and readability

--- 933.number-of-recent-calls.py
+++ 933.number-of-recent-calls.py
@@ -4,27 +4,27 @@
 # [933] Number of Recent Calls
 #
 
+
 # @lc code=start
+from collections import deque
+
+
 class RecentCounter:
 
     def __init__(self):
-        # init a queue store the current timeframe
-        # ex: after ping t, the queue only contain history ping between t-3000 ~ t
-        self.q = []
-        
+        # init a queue store timestamp for each past request
+        self.pastReq = deque([])
 
     def ping(self, t: int) -> int:
-        self.q.append(t)
-        while self.q:
-            if self.q[0]>=t-3000:
-                break
-            self.q.pop(0)
-        return len(self.q)
-        
+        # push to pastReq
+        # keep popping the first item if it's expired (<t-3000)
+        self.pastReq.append(t)
+        while self.pastReq and t - 3000 > self.pastReq[0]:
+            self.pastReq.popleft()
+        return len(self.pastReq)
 
 
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
 # param_1 = obj.ping(t)
 # @lc code=end
-
