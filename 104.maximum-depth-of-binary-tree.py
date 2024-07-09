4202699c018e10913f3a0b5e7cf4d4b2b760ed16
swh00tw <a6140000@gmail.com>
2024-07-08 23:00:50 -0400

    Refactor maxDepth method in Solution class for improved efficiency and readability

--- 104.maximum-depth-of-binary-tree.py
+++ 104.maximum-depth-of-binary-tree.py
@@ -4,6 +4,7 @@
 # [104] Maximum Depth of Binary Tree
 #
 
+
 # @lc code=start
 # Definition for a binary tree node.
 # class TreeNode:
@@ -12,16 +13,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.treeDepth = 0
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
         self.traverse(root, 1)
-        return self.treeDepth
+        return self.ans
+
     def traverse(self, node, depth):
         if node:
-            self.treeDepth = max(self.treeDepth, depth)
-            self.traverse(node.left, depth+1)
-            self.traverse(node.right, depth+1)
-        
-# @lc code=end
+            self.ans = max(self.ans, depth)
+            self.traverse(node.left, depth + 1)
+            self.traverse(node.right, depth + 1)
 
+
+# @lc code=end
