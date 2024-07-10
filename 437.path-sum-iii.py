e13531dbc8bf790b06f72a4b35cb18b17ccdddef
swh00tw <a6140000@gmail.com>
2024-07-09 22:55:14 -0400

    Refactor pathSum method in Solution class for improved efficiency and readability

--- 437.path-sum-iii.py
+++ 437.path-sum-iii.py
@@ -4,6 +4,7 @@
 # [437] Path Sum III
 #
 
+
 # @lc code=start
 # Definition for a binary tree node.
 # class TreeNode:
@@ -11,23 +12,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        # pathSum(root.left, targetSum-root.val) + pathSum(root.right, targetSum-root.val)
+        # + pathSum(root.left, targetSum) + pathSum(root.right, targetSum)
+        # + 1 if root.val == targetSum
+        self.sum = targetSum
         if not root:
             return 0
-        return self.pathSum(root.left, targetSum)+self.pathSum(root.right, targetSum)+self.traverse(root, targetSum)
-    
-    def traverse(self, node, targetSum): # find how many paths begin from "node" has sum equal to targetSum
+        return (
+            self.countPathSum(root, targetSum)
+            + self.pathSum(root.left, targetSum)
+            + self.pathSum(root.right, targetSum)
+        )
+
+    def countPathSum(self, node: Optional[TreeNode], targetSum):
         if not node:
             return 0
         ans = 0
         if node.val == targetSum:
             ans += 1
-        # the answer also consists of left child and right child
-        ans += self.traverse(node.left, targetSum-node.val)
-        ans += self.traverse(node.right, targetSum-node.val)
-        return ans
-        
+        return (
+            ans
+            + self.countPathSum(node.left, targetSum - node.val)
+            + self.countPathSum(node.right, targetSum - node.val)
+        )
+
 
 # @lc code=end
-
