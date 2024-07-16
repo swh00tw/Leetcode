cf7218b1bdc354fbd26501801201e30687fb07bb
swh00tw <a6140000@gmail.com>
2024-07-15 22:48:23 -0400

    Refactor canVisitAllRooms method in Solution class for improved efficiency and readability

--- 841.keys-and-rooms.py
+++ 841.keys-and-rooms.py
@@ -4,21 +4,21 @@
 # [841] Keys and Rooms
 #
 
+
 # @lc code=start
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         n = len(rooms)
-        visited = [False]*n
-        q = [0]
+        visited = [False] * n
         visited[0] = True
-        while q:
-            roomIdx = q.pop(0)
-            keys = rooms[roomIdx]
-            for key in keys:
-                if visited[key]==False:
-                    visited[key]=True
-                    q.append(key)
+        queue = rooms[0]
+        while queue:
+            idx = queue.pop(0)
+            if visited[idx] is True:
+                continue
+            queue.extend(rooms[idx])
+            visited[idx] = True
         return all(visited)
-        
-# @lc code=end
 
+
+# @lc code=end
