3f79ee679e7ccbb265c59fe33c66e4082514589c
swh00tw <a6140000@gmail.com>
2024-07-15 22:48:29 -0400

    Refactor UnionFind class for improved efficiency and readability

--- 547.number-of-provinces.py
+++ 547.number-of-provinces.py
@@ -4,39 +4,47 @@
 # [547] Number of Provinces
 #
 
+
 # @lc code=start
 class UnionFind:
     def __init__(self, n):
         self.root = list(range(n))
-        self.rank = [0]*n
+        self.rank = [0] * n
+
     def find(self, n):
-        if self.root[n]!=n:
+        if self.root[n] != n:
             self.root[n] = self.find(self.root[n])
         return self.root[n]
+
     def union(self, x, y):
-        rx, ry = self.find(x), self.find(y)
-        if rx!=ry:
-            if self.rank[rx]>self.rank[ry]:
-                self.root[ry] = rx
-            elif self.rank[ry]>self.rank[rx]:
+        rx = self.find(x)
+        ry = self.find(y)
+        if rx != ry:
+            if self.rank[rx] < self.rank[ry]:
                 self.root[rx] = ry
+            elif self.rank[ry] < self.root[rx]:
+                self.root[ry] = rx
             else:
                 self.root[rx] = ry
                 self.rank[ry] += 1
-    def getDisjointSets(self):
-        res = set()
-        for i in range(len(self.root)):
-            res.add(self.find(i))
-        return res
+
+    def getNumProvinces(self):
+        n = len(self.root)
+        for i in range(n):
+            self.find(i)
+        rootSet = set(self.root)
+        return len(rootSet)
+
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
         uf = UnionFind(n)
         for i in range(n):
-            for j in range(i, n):
-                if i!=j and isConnected[i][j]==1:
+            for j in range(n):
+                if i != j and isConnected[i][j] == 1:
                     uf.union(i, j)
-        return len(uf.getDisjointSets())
+        return uf.getNumProvinces()
+
 
 # @lc code=end
-
