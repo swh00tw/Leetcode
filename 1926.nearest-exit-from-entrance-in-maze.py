0ab41bf5b830d1bf3e9297731dbc8e2b574f4f46
swh00tw <a6140000@gmail.com>
2024-07-16 22:32:18 -0400

    Refactor Maze class for improved efficiency and readability

--- 1926.nearest-exit-from-entrance-in-maze.py
+++ 1926.nearest-exit-from-entrance-in-maze.py
@@ -5,45 +5,52 @@
 #
 
 # @lc code=start
+from collections import deque
+
+
+class Maze:
+    def __init__(self, map):
+        self.n = len(map)
+        self.m = len(map[0])
+        self.map = map
+
+    def isBoundary(self, i, j):
+        if i == 0 or j == 0:
+            return True
+        if i == self.n - 1 or j == self.m - 1:
+            return True
+        return False
+
+    def getNearestExit(self, i, j):
+        # return number of steps to nearest exit
+        # if no, return -1
+        # bfs
+        # use a variable to keep track of steps need to reach nearest entrance
+        # early stop. if traverse to boundary, stop since further traveral will only increase number of steps
+        ans = float("inf")
+        q = deque([(i, j, 0)])
+        visited = [[False] * self.m for _ in range(self.n)]
+        visited[i][j] = True
+        delta = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        while q:
+            x, y, steps = q.popleft()
+            if self.isBoundary(x, y) and (x, y) != (i, j):
+                ans = min(ans, steps)
+                continue
+            for dx, dy in delta:
+                nx = x + dx
+                ny = y + dy
+                if 0 <= nx < self.n and 0 <= ny < self.m:
+                    if visited[nx][ny] is False and self.map[nx][ny] == ".":
+                        visited[nx][ny] = True
+                        q.append((nx, ny, steps + 1))
+        return ans if ans < float("inf") else -1
+
+
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        nearestExitDistance = inf
-        m = len(maze[0])
-        n = len(maze)
-        def isExit(x, y):
-            if x==entrance[0] and y==entrance[1]:
-                return False
-            if x==0 or x==n-1:
-                return maze[x][y]=="."
-            if y==0 or y==m-1:
-                return maze[x][y]=="." 
-        # bfs
-        distance = [[inf]*m for _ in range(n)]
-        distance[entrance[0]][entrance[1]] = 0
-        def isUnvisitedCell(x, y):
-            return maze[x][y]=="." and distance[x][y]==inf
-        q = [entrance]
-        while q:
-            x, y = q.pop(0)
-            if isExit(x, y):
-                nearestExitDistance = min(nearestExitDistance, distance[x][y])
-            # up
-            if x-1>=0 and isUnvisitedCell(x-1, y):
-                distance[x-1][y] = distance[x][y]+1
-                q.append([x-1, y])
-            # down
-            if x+1<n and isUnvisitedCell(x+1, y):
-                distance[x+1][y] = distance[x][y]+1
-                q.append([x+1, y])
-            # left
-            if y-1>=0 and isUnvisitedCell(x, y-1):
-                distance[x][y-1] = distance[x][y]+1
-                q.append([x, y-1])
-            # right
-            if y+1<m and isUnvisitedCell(x, y+1):
-                distance[x][y+1] = distance[x][y]+1
-                q.append([x, y+1])
-        return nearestExitDistance if nearestExitDistance!=inf else -1
-                
-# @lc code=end
+        m = Maze(maze)
+        return m.getNearestExit(entrance[0], entrance[1])
 
+
+# @lc code=end
