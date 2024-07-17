5f9bae3d1fd80eb0273c6e559f7f012836b43842
swh00tw <a6140000@gmail.com>
2024-07-16 22:50:11 -0400

    Refactor orangesRotting method in Solution class for improved efficiency and readability

--- 994.rotting-oranges.py
+++ 994.rotting-oranges.py
@@ -6,51 +6,49 @@
 
 # @lc code=start
 # faster sol: https://leetcode.com/problems/rotting-oranges/solutions/563686/python-clean-well-explained-faster-than-90/?envType=study-plan-v2&envId=leetcode-75
+from collections import deque
+
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        # BFS from each rotten orange as source
-        # create a m x n table to store the time that orange got rotten
-        self.m = len(grid[0])
-        self.n = len(grid)
-        self.grid = grid
-
-        self.rottenTime = [[inf]*self.m for _ in range(self.n)]
-        self.rottenOranges = []
-        totalOranges = 0
-        for i in range(self.n):
-            for j in range(self.m):
-                if grid[i][j]!=0:
-                    totalOranges+=1
-                if grid[i][j]==2:
-                    self.rottenOranges.append((i, j))
-        if totalOranges==0:
+        # n = len(grid)
+        # m = len(grid[0])
+        # count how many rotten oranges and put the rotten ones' position to an array
+        # count total oranges
+        # for each rotten orange, run BFS simultanenuously
+        # and after BFS finish, return how many minutes past, if not all oranges become rotten, return -1
+        # when we run BFS, we can keep track of the timing for each orange become rotten
+        n = len(grid)
+        m = len(grid[0])
+        finishTime = -1
+        total = 0
+        rotten = 0
+        q = deque([])
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 2:
+                    q.append((i, j, 0))
+                    rotten += 1
+                if grid[i][j] != 0:
+                    total += 1
+        if rotten == total:
             return 0
-        for src in self.rottenOranges:
-            self.BFS(src)
-        ans = -1
-        for i in range(self.n):
-            for j in range(self.m):
-                if grid[i][j]!=0:
-                    if self.rottenTime[i][j]==inf:
-                        return -1
-                    ans = max(ans, self.rottenTime[i][j])
-        return ans
-                
-
-    def BFS(self, src):
-        distance = [[inf]*self.m for _ in range(self.n)]
-        distance[src[0]][src[1]] = 0
-        q = [src]
-        adj = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        graph = [[grid[x][y] for y in range(m)] for x in range(n)]
+        delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         while q:
-            x, y = q.pop(0)
-            # if it can update self.rottenTime successfully, keep going, else, early stop
-            if distance[x][y]<self.rottenTime[x][y]:
-                self.rottenTime[x][y] = distance[x][y]
-                for i, j in adj:
-                    if 0<=x+i<self.n and 0<=y+j<self.m and self.grid[x+i][y+j]!=0 and distance[x+i][y+j]==inf:
-                        distance[x+i][y+j] = distance[x][y]+1
-                        q.append((x+i, y+j))
-        
-# @lc code=end
+            x, y, t = q.popleft()
+            finishTime = max(finishTime, t)
+            for dx, dy in delta:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+                    graph[nx][ny] = 2
+                    rotten += 1
+                    q.append((nx, ny, t + 1))
 
+        if rotten == total:
+            return finishTime
+        return -1
+        # TC: O(mn)
+
+
+# @lc code=end
