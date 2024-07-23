23d1bee2eb059dc9fed923033a22837fbadcbedf
swh00tw <a6140000@gmail.com>
2024-07-23 01:33:40 -0400

    Refactor maxScore method in Solution class for improved efficiency and readability

--- 2542.maximum-subsequence-score.py
+++ 2542.maximum-subsequence-score.py
@@ -7,26 +7,27 @@
 # @lc code=start
 # ref: https://leetcode.com/problems/maximum-subsequence-score/editorial/
 import heapq
+
+
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
         res = -1
-        pairs = sorted(list(zip(nums1, nums2)), key=lambda x:-x[1])
-        topNumbers = [p[0] for p in pairs[:k-1]] # keep track of top k-1 numbers currently discovered
+        pairs = sorted(list(zip(nums1, nums2)), key=lambda x: -x[1])
+        topNumbers = [
+            p[0] for p in pairs[: k - 1]
+        ]  # keep track of top k-1 numbers currently discovered
         heapq.heapify(topNumbers)
         sumOfHeap = sum(topNumbers)
 
-        for pair in pairs[k-1:]:
+        for pair in pairs[k - 1 :]:
             multiplier = pair[1]
             s = pair[0] + sumOfHeap
-            res = max(res, s*multiplier)
+            res = max(res, s * multiplier)
             # update topNumbers
             heapq.heappush(topNumbers, pair[0])
             deleteNum = heapq.heappop(topNumbers)
-            sumOfHeap = sumOfHeap+pair[0]-deleteNum
+            sumOfHeap = sumOfHeap + pair[0] - deleteNum
         return res
 
 
-
-        
 # @lc code=end
-
