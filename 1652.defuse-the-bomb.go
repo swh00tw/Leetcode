ad25d05561bb0f64d0ce48f907f0958e91b055b2
swh00tw <a6140000@gmail.com>
2024-11-18 15:15:52 -0500

    Add solution for LeetCode problem 1652: Defuse the Bomb

---
+++ 1652.defuse-the-bomb.go
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=1652 lang=golang
+ *
+ * [1652] Defuse the Bomb
+ */
+
+// @lc code=start
+package main
+
+import "slices"
+
+func decrypt(code []int, k int) []int {
+	if k < 0 {
+		slices.Reverse(code)
+		res := decrypt(code, -k)
+		slices.Reverse(res)
+		return res
+	}
+	if k == 0 {
+		return make([]int, len(code))
+	}
+	// if k >= 0
+	sum := 0
+	for _, v := range code {
+		sum += v
+	}
+	n := len(code)
+	round := k / n
+	k = k % n
+
+	res := make([]int, n)
+	for i, _ := range code {
+		res[i] = round * sum
+	}
+	nextK := 0
+	for i := 0; i < k; i++ {
+		idx := i + 1
+		nextK += code[idx%n]
+	}
+	for i, _ := range code {
+		res[i] += nextK
+		// update nextK
+		nextK -= code[(i+1)%n]
+		nextK += code[(i+k+1)%n]
+	}
+	return res
+}
+
+// @lc code=end
