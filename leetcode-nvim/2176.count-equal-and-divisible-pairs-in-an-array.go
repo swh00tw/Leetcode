36bf56ce90fff3c34c0e0b4b63113270a4b07f5e
swh00tw <a6140000@gmail.com>
2025-04-16 21:58:50 -0400

    chore: daily

---
+++ leetcode-nvim/2176.count-equal-and-divisible-pairs-in-an-array.go
@@ -0,0 +1,17 @@
+package main
+
+// @leet start
+func countPairs(nums []int, k int) int {
+	count := 0
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] == nums[j] && (i*j)%k == 0 {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+// @leet end
+
---
+++ leetcode-nvim/2179.count-good-triplets-in-an-array.go
@@ -0,0 +1,11 @@
+package main
+
+// @leet start
+func goodTriplets(nums1 []int, nums2 []int) int64 {
+	// need hashmap for each arr to store val to idx
+	// use 2 for loops to find the first two nums,
+	// count += the shared nums on the right side of num2 (need to build a hashmap for this)
+}
+
+// @leet end
+
